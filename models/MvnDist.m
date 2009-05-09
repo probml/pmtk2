94e0778512aa74aa2c9e10550cc70b6e92bd2b98
mattbdunham <mattbdunham@498ffb9c-3bfa-11de-801c-c7cb65f9a556>
2009-05-09 00:54:50 +0000

    Added MvnDist template
    git-svn-id: https://pmtk2.googlecode.com/svn/trunk@10 498ffb9c-3bfa-11de-801c-c7cb65f9a556

---
+++ models/MvnDist.m
@@ -0,0 +1,43 @@
+classdef MvnDist < MultivarDist
+%MVNDIST Multivariate Normal Distribution
+    
+    properties
+        params;
+    end
+    
+    methods
+        
+        function model = MvnDist(varargin)
+            
+        end
+        
+        function logp = logprob(model,varargin)
+            
+        end
+       
+        function S = sample(model,varargin)
+            
+        end
+        
+        function m = mean(model)
+            
+        end
+        
+        function m = mode(model)
+            
+        end
+        
+        function c = cov(model)
+            
+        end
+        
+        function model = fit(model,varargin)
+            
+        end
+         
+       
+       
+    end
+    
+end
+
