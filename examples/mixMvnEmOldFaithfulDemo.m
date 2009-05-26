a7656812cb4c3ee4010e6dbf046c6d87bf011f59
murphyk2@gmail.com <murphyk2@gmail.com@498ffb9c-3bfa-11de-801c-c7cb65f9a556>
2009-05-26 00:48:58 +0000

    mixMvnEMoldFaithfulDemo now matches the ppt slides: points are color coded based on their hard cluster assignement
    git-svn-id: https://pmtk2.googlecode.com/svn/trunk@94 498ffb9c-3bfa-11de-801c-c7cb65f9a556

--- examples/mixMvnEmOldFaithfulDemo.m
+++ examples/mixMvnEmOldFaithfulDemo.m
@@ -4,16 +4,19 @@
 load oldFaith;
 D = DataTable(X);
 m  = MixMvn('-nmixtures',2);
-%m.fitEng.verbose = true;
+m.fitEng.verbose = true;
 m = fit(m,D);
-post = inferLatent(m,D);
-map = mode(post);
-plot(X(:,1),X(:,2),'.');
+% Desired
+%[Zhat, post] = inferLatent(m,D);
+%assertIsequal(Zhat,  mode(post));
+% Current
+pZ = inferLatent(m,D);
+Zhat = mode(pZ);
 hold on;
-plotPdf(m.mixtureComps{1});
-plotPdf(m.mixtureComps{2});
 colors = {'g', 'b'};
 for c=1:2
-  ndx = find((map==c));
+  plotPdf(m.mixtureComps{c});
+  ndx = find((Zhat==c));
   plot(X(ndx,1),X(ndx,2), sprintf('%s.', colors{c}));
 end
+
