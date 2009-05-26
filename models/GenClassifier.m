f3ef8fa53286808b851693f90c5d2a5c3142d031
mattbdunham <mattbdunham@498ffb9c-3bfa-11de-801c-c7cb65f9a556>
2009-05-26 16:52:32 +0000

    Added Generative Classifier
    git-svn-id: https://pmtk2.googlecode.com/svn/trunk@96 498ffb9c-3bfa-11de-801c-c7cb65f9a556

--- models/GenClassifier.m
+++ models/GenClassifier.m
@@ -15,26 +15,42 @@ classdef GenClassifier < CondModel
 	methods
 
 		function model = GenClassifier(varargin)
-		%
+            [model.params.classConditionals,model.prior,template,nclasses] = processArgs(varargin,'-classConditionals',{},'-classPrior',DiscreteDist(),'-template',[],'-nclasses',[]);
+            if ~isempty(template) && ~isempty(nclasses), model.params.classConditionals = copy(template,nclasses); end
+            model = initialize(model);
 		end
 
 
-		function computeMapOutput(model,varargin)
-		%
-			notYetImplemented('GenClassifier.computeMapOutput()');
+		function model = fit(model,varargin)
+            D = processArgs(varargin,'+-data',DataTableXY());
+            X = D.X; y = D.y;
+            model.prior = fit(model.prior, '-data',  colvec(y));
+            classConds = model.params.classConditionals;
+            support = model.prior.support;
+            for c=1:numel(classConds)
+              classConds{c} = fit(classConds{c},'-data',X(y==support(c),:));
+            end
+            model.params.classConditionals = classConds;
+            model = initialize(model);
 		end
 
 
-		function fit(model,varargin)
-		%
-			notYetImplemented('GenClassifier.fit()');
-		end
-
-
-		function inferOutput(model,varargin)
-		%
-			notYetImplemented('GenClassifier.inferOutput()');
-		end
+        function [yHat,pY] = inferOutput(model,D)
+            X = D.X;
+            logpy = log(pmf(model.prior));
+            n = ncases(D);
+            classConds = model.params.classConditionals;
+            C = numel(classConds);
+            L = zeros(n,C);
+            for c=1:C
+                L(:,c) = logPdf(classConds{c},X) + logpy(c);
+            end;
+            post = exp(normalizeLogspace(L));
+            yHat = maxidx(post,[],2);
+            if nargout > 2
+                pY = DiscreteDist(post', '-support', model.prior.support);
+            end
+        end
 
 
 		function logPdf(model,varargin)
@@ -49,7 +65,19 @@ classdef GenClassifier < CondModel
 		end
 
 
-	end
+    end
+    
+    methods(Access = 'protected')
+        function model = initialize(model)
+            dof= model.prior.dof;
+            classConds = model.params.classConditionals;
+            for i=1:numel(classConds)
+                dof = dof + classConds{i}.dof;
+            end
+            model.dof = dof;
+        end
+        
+    end
 
 
 end
