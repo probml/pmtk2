678ff27984bd922067ace987f1452f63f71e37ac
murphyk2@gmail.com <murphyk2@gmail.com@498ffb9c-3bfa-11de-801c-c7cb65f9a556>
2009-05-27 20:21:06 +0000

    git-svn-id: https://pmtk2.googlecode.com/svn/trunk@114 498ffb9c-3bfa-11de-801c-c7cb65f9a556

--- util/graphics/printPmtkFigure.m
+++ util/graphics/printPmtkFigure.m
@@ -1,17 +1,19 @@
 function printPmtkFigure(filename)
 % print current figure to specified file in .eps and .pdf formats
 
-if true % set to false to turn off printing
-  printFolder = '/ubc/cs/home/n/nevek/fig';
-  %printFolder = 'C:\kmurphy\pmtkLocal\figures/';
+if false % set to false to turn off printing
+  %printFolder = '/ubc/cs/home/n/nevek/fig';
+  printFolder = 'C:\kmurphy\pmtkLocal\figures/';
   pdfcrop;
   opts = struct('Color', 'rgb', 'Resolution', 1200);
   try
+    fprintf('printing to %s/%s\n', printFolder, filename);
     exportfig(gcf, sprintf('%s/%s.eps', printFolder, filename), opts, 'Format', 'eps' );
     exportfig(gcf, sprintf('%s/%s.pdf', printFolder, filename), opts, 'Format', 'pdf' );
     %export_fig(fullfile(printFolder, sprintf('%s.pdf', filename)), '-pdf');
     %export_fig(fullfile(printFolder, sprintf('%s.eps', filename)), '-eps');
   catch ME
+    % could silently return instead...
     fprintf('could not print to %s/%s; edit printPmtkFigure.m\n', printFolder, filename);
   end
 end
